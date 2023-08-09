94366dab4abe1d0132b7fdde24b692e693bcf8da
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-08-08 20:37:05 -0400

    Add constraints for France zip

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ function checkZIP() {
             "^(CH-)?\\d{4}$",
             "Switzerland ZIPs must have exactly 4 digits: e.g. CH-1950 or 1950",
         ],
-        
+        fr: [
+            "^(F-)?\\d{5}$",
+            "France ZIPs must have exactly 5 digits: e.g. F-75012 or 75012",
+        ],
     }
 }
